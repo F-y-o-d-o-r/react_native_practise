616c255fcefaef3ef58a7757a7698a164448ae73
Fyodor <fyodor.work@gmail.com>
2018-09-19 17:54:32 +0300

    carousel

--- App.js
+++ App.js
@@ -1,49 +1,11 @@
-import React from 'react';
-import { FlatList, ActivityIndicator, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 
-export default class FetchExample extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isLoading: true };
-  }
-
-  componentDidMount() {
-    return fetch('https://facebook.github.io/react-native/movies.json')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        this.setState(
-          {
-            isLoading: false,
-            dataSource: responseJson.movies
-          },
-          function() {}
-        );
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
-
+export default class HelloWorldApp extends Component {
   render() {
-    if (this.state.isLoading) {
-      return (
-        <View style={{ flex: 1, padding: 20 }}>
-          <ActivityIndicator />
-        </View>
-      );
-    }
-
     return (
-      <View style={{ flex: 1, paddingTop: 20 }}>
-        <FlatList
-          data={this.state.dataSource}
-          renderItem={({ item }) => (
-            <Text>
-              {item.title}, {item.releaseYear}
-            </Text>
-          )}
-          keyExtractor={({ id }, index) => id}
-        />
+      <View>
+        <Text>Hello world!</Text>
       </View>
     );
   }
